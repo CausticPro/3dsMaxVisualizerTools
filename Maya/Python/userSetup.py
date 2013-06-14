bca055e062bd2db8c19a88af03dce2ce02758d35
Kevin Bjorke <kevin.bjorke@ingtec.com>
2013-06-14 14:06:42 -0700

    removed Maya-related components

--- Maya/Python/userSetup.py
+++
@@ -1,6 +0,0 @@
-"""
-Sample userSetup that loads VisualizerTools. Copy this file to ..\Documents\maya\scripts\
-    if you don't already have a setup file, or just add the 2 lines below to your existing script.
-"""
-from VisualizerToolsSetup import *
-VisualizerToolsSetup()
